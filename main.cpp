c2dfad91ea6fb4250ffe258f8cc4a1f892390ac4
Jonathan Shnayder <rangersps42@gmail.com>
2021-10-22 18:17:15 -0400

    Wrote solve function and added test cases

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
-
+#include <vector>
 #include "funcs.h"
+#include <cctype>
+#include <cmath>
 
-int main()
-{
-  return 0;
+int main(){
+	//std::string s = "Jhsjbshavy lujyfwaz lualylk alea if bzpun Jhlzhy jpwoly. Uvu-hswohilapj zftivsz (kpnpaz, dopalzwhjlz, laj.) hyl uva ayhuzmvytlk.";
+	//std::cout << "Input: \n" << s << std::endl;
+
+	//std::cout << "Output: \n" <<  solve(s) << std::endl;
+	
+	
+	return 0;
 }
